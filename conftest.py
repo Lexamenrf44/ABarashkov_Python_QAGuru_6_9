6c2a4f6520baef68615c93e24f18a91900327c9a
ABarashkov <alexander.stcstudent@gmail.com>
2023-07-08 18:28:03 +0400

    initial commit - setup

--- conftest.py
+++ conftest.py
@@ -3,6 +3,8 @@ from selene.support.shared import browser
 
 
 @pytest.fixture(scope='function', autouse=True)
+
+# Basic setup
 def browser_setup():
     browser.config.window_height = '1680'
     browser.config.window_width = '1050'
